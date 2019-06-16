33c2043a552111db7c12d876f881d3aaae796c4b
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-06-16 17:50:47 -0400

    Working on using matrix two dimensional array

--- pathfinder.py
+++ pathfinder.py
@@ -1,46 +1,38 @@
 from PIL import Image
+import os
 
-def read_line_of_ints(text):
-    """Given a string with integers in it, return a list of those integers."""
-    ints = []
-    ints_as_strs = split_line(text)
-
-    # equivalent code to the for loop below
-    # index = 0
-    # while index < len(ints_as_strs):
-    #     int_as_str = ints_as_strs[index]
-    #     ints.append(int(int_as_str))
-    #     index += 1
-
-    for int_as_str in ints_as_strs:
-        ints.append(int(int_as_str))
-    return ints
+from numpy import *
 
 
-def split_line(line):
-    return line.split()
+# def read_line_of_ints(text):
+#     """Given a string with integers in it, return a list of those integers."""
+#     ints = []   
+#     ints_as_strs = split_line(text)
+
+
+#     for int_as_str in ints_as_strs:
+#         ints.append(int(int_as_str))
+#     print (ints)
 
 
 def read_file_into_list(filename):
-    """Given a file, return a list of each line in the file as a string."""
-    with open(filename) as file:
+    # """Given a file, return a list of each line in the file as a string."""
+    with open("elevation_small.txt") as file:
         return file.readlines()
-
+        
+#read_file_into_list("elevation_small.txt")
 
 def read_file_into_ints(filename):
-    """Given a filename, read that file and then convert it to a list
-    of lists of ints. Example:
-    We have a file with these contents:
-    1 2
-    3 4
-    The return value would be [[1, 2], [3, 4]]
-    """
-    lines = read_file_into_list(filename)
-
     list_of_lists = []
+    lines = read_file_into_list(filename)
     for line in lines:
-        list_of_lists.append(read_line_of_ints(line))
-    return list_of_lists
+        list_of_lists.append(read_line_of_ints(lines))
+        return list_of_lists
+
+read_file_into_ints("elevation_small.txt")
+
+#read_file_into_list("elevation_small.txt")
+#return list_of_lists
 
 
 class ElevationMap:
@@ -73,24 +65,38 @@ class ElevationMap:
 
         return (elevation - min_elevation) / (max_elevation - min_elevation)
 
+    def draw_my_map(filename, width, height):
+        image = Image.new('RGBA', (width, height), 'white')
+        for x in range(width):
+            for y in range(height):
+              '''image.putpixel((x, y), (int(x / width * 255),))'''
+              image.save("map.png")
+              draw_test = ImageDraw.Draw(image)
+              image.show()
+              print("done")
 
-def draw_grayscale_gradient(filename, width, height):
-    image = Image.new(mode='L', size=(width, height))
-    for x in range(width):
-        for y in range(height):
-            image.putpixel((x, y), (int(x / width * 255),))
-    image.save(filename)
+
+
+# # def draw_my_map(filename, width, height):
+# #     image = Image.new(mode='L', size=(width, height))
+# #     for x in range(width):
+# #         for y in range(height):
+# #             image.putpixel((x, y), (int(x / width * 255),))
+# #             image.save("map.png")
+
+
+
 
 
 if __name__ == "__main__":
-    my_str = "10 12 9 345 2 78"
-    read_line_of_ints(my_str)
+    # my_str = "list_of_lists"
+    # read_line_of_ints(list_of_lists)
 
-    elevations = read_file_into_ints('elevation_test.txt')
+    elevations = read_file_into_ints("elevation_small.txt")
 
-    e_map = ElevationMap(elevations)
-    print(e_map.intensity_at_coordinate(1, 2))
+    my_map = ElevationMap(elevations)
+    print(my_map.intensity_at_coordinate(1, 2))
 
-    draw_grayscale_gradient('test.png', 400, 400)
+    #draw_my_map('map.png', 400, 400)
 
 
---
+++ test.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+m = ([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+    
+
+print(m[2])
+
+# def lines()
+# with open("elevation_small.txt") as file:
+#     return (file.readlines())
+
+# lines = lines()
+
+# lines.spilt()
+# print lines
+
+        
+# #read_file_into_list("elevation_small.txt")
